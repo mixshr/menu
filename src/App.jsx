26ba617c1a67465980310e46c412fe2956c7c8b9
mihailsarenko <mixail.sharenko@gmail.com>
2024-05-26 19:41:11 +0500

    Any way commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(
+    items.map((item) => item.category)
+)]
 function App() {
   const [menu, setMenu] = useState(items);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenu(items);
+    } else {
+      const newMenu = menu.filter((item) => category === item.category);
+      setMenu(newMenu);
+    }
+  }
 
   return <main>
     <h2 className='title'>Our menu</h2>
     <div className='underline'></div>
     <section className='menu'>
-      <Menu items={menu}/>
+      <Categories categories={allCategories} filterItems={filterItems}/>
+      <Menu items={menu} />
     </section>
   </main>;
 }
